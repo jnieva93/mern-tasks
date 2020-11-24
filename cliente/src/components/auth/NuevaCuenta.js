27bdcfed8d2fc3f4f6ab08e6529f2337988d0912
jnieva <josenieva93@gmail.com>
2020-11-24 20:17:05 -0300

    Add NuevaCuenta State

--- cliente/src/components/auth/NuevaCuenta.js
+++ cliente/src/components/auth/NuevaCuenta.js
@@ -1,8 +1,90 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const NuevaCuenta = () => {
+  const [usuario, setUsuario] = useState({ nombre: '', email: '', password: '', confirmar: '' });
+
+  const { nombre, email, password, confirmar } = usuario;
+  
+  const handleChange = e => {
+    setUsuario({
+      ...usuario,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
+
   return (
-    <h1>Desde NuevaCuenta</h1>
+    <div className='form-usuario'>
+      <div className='contenedor-form sombra-dark'>
+        <h1>Obtener una Cuenta</h1>
+
+        <form onSubmit={handleSubmit}>
+          <div className='campo-form'>
+            <label htmlFor='nombre'>Nombre</label>
+            <input
+              type='text'
+              id='nombre'
+              name='nombre'
+              placeholder='Tu Nombre'
+              value={nombre}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className='campo-form'>
+            <label htmlFor='email'>Email</label>
+            <input
+              type='email'
+              id='email'
+              name='email'
+              placeholder='Tu Email'
+              value={email}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className='campo-form'>
+            <label htmlFor='password'>Password</label>
+            <input
+              type='password'
+              id='password'
+              name='password'
+              placeholder='Tu Password'
+              value={password}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className='campo-form'>
+            <label htmlFor='confirmar'>Confirmar Password</label>
+            <input
+              type='password'
+              id='confirmar'
+              name='confirmar'
+              placeholder='Repite tu Password'
+              value={confirmar}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className='campo-form'>
+            <input
+              type='submit'
+              className='btn btn-primario btn-block'
+              value='Registrarme'
+            />
+          </div>
+        </form>
+
+        <Link to='/' className='enlace-cuenta'>
+          Volver a Iniciar Sesión
+        </Link>
+      </div>
+    </div>
   );
 }
  

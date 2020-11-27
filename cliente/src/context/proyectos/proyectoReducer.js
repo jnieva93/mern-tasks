9c134de8de806a9b081f68af42e424878021e863
jnieva <josenieva93@gmail.com>
2020-11-27 19:02:46 -0300

    Fix proyectoReducer warnings

--- cliente/src/context/proyectos/proyectoReducer.js
+++ cliente/src/context/proyectos/proyectoReducer.js
@@ -1,6 +1,6 @@
 import { FORMULARIO_PROYECTO } from "../../types";
 
-export default (state, action) => {
+const proyectoReducer = (state, action) => {
   switch (action.type) {
     case FORMULARIO_PROYECTO:
       return {
@@ -9,5 +9,7 @@ export default (state, action) => {
       }
     default:
       return state;
-  }
-}
+  };
+};
+
+export default proyectoReducer;

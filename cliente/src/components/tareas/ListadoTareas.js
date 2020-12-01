ebe2d7718085bb6d25affa0ba72da2b5b4c8f668
jnieva93 <josenieva93@gmail.com>
2020-12-01 14:53:50 -0300

    Show Project name when clicked

--- cliente/src/components/tareas/ListadoTareas.js
+++ cliente/src/components/tareas/ListadoTareas.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import proyectoContext from '../../context/proyectos/proyectoContext';
 import Tarea from './Tarea';
 
 const ListadoTareas = () => {
@@ -8,10 +9,16 @@ const ListadoTareas = () => {
     { nombre: 'Elegir Plataformas de Pago', completa: false },
     { nombre: 'Elegir Hosting', completa: true }
   ];
+
+  const proyectosContext = useContext(proyectoContext);
+
+  const { proyecto } = proyectosContext;
+
+  if (!proyecto) return <h2>Selecciona un Proyecto</h2>;
   
   return (
     <>
-      <h2>Proyecto: Tienda Virtual</h2>
+      <h2>Proyecto: {proyecto.nombre}</h2>
 
       <ul className='listado-tareas'>
         {tareasProyecto.length === 0

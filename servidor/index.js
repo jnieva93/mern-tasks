fc71094152e1f8f1e00ecb67d34b3e5dfd40fac2
jnieva <josenieva93@gmail.com>
2020-12-21 20:57:37 -0300

    Create Express server

--- servidor/index.js
+++ servidor/index.js
@@ -1,1 +1,11 @@
-console.log("Desde Index");
+const express = require('express');
+
+// Creación del servidor
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+// Inicia app
+app.listen(PORT, () => {
+  console.log(`El servidor está funcionando en el puerto ${PORT}`);
+});
